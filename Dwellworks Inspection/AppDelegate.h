c451be93c35d8910171145c1d14982fc87e5f46b
Austin Emser <austinemser@Austins-MacBook-Pro.local>
2014-01-29 14:01:45 -0500

    Initial Commit

---
+++ Dwellworks Inspection/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dwellworks Inspection
+//
+//  Created by Austin Emser on 1/29/14.
+//  Copyright (c) 2014 Austin Emser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dwellworks Inspection/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Dwellworks Inspection
+//
+//  Created by Austin Emser on 1/29/14.
+//  Copyright (c) 2014 Austin Emser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
